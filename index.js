b26bfde28529b713ce13a352f2c654bbbb821cef
Gamaliel <angel.gamaliel@hotmail.com>
2021-10-08 21:57:31 -0500

    Set up to work

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//
+//Regresar el resultado por medio innherText en el div con id="Resultados":
+//****Ingreso.- onclick="ingresar()" submit name--id="Ingreso"
+//       es decir se ejecutara ingresar() al hacer el click en el botton con id=Ingreso
+//****Retirar.- onclick="retirar()" submit name--id="Retiro"
+//       es decir se ejecutara ingresar() al hacer el click en el botton con id=Retiro
+//  -----------------------------resultados= linea **1**---------------------------------------
+var x = ["", 0];
+
+function ingresar(event) {
+    //esto es para que no se reinicio tu submit
+    event.preventDefault();
+    //Code here: Alvaro Fierro
+    //Se te dara un array con nombre x donde contenga [nombre=string,saldo=Number];
+    //Como hacerlo?= estan las indicaciones en la linea **2**
+
+}
+
+function retirar(event) {
+    //esto es para que no se reinicio tu submit
+    event.preventDefault();
+    //Code here Luis moreno
+    //Se te dara un array con nombre x donde contenga [nombre=string,saldo=Number];
+    //el resultado estan las indicaciones en la linea **4**
+
+}
