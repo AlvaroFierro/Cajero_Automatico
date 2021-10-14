919b87faf59da0b4e03a92ffb66618de50efb35c
Gamaliel <angel.gamaliel@hotmail.com>
2021-10-13 19:19:05 -0500

    Check it of emails done

--- index.js
+++ index.js
@@ -4,9 +4,14 @@
 //****Retirar.- onclick="retirar()" submit name--id="Retiro"
 //       es decir se ejecutara ingresar() al hacer el click en el botton con id=Retiro
 //  -----------------------------resultados= linea **1**---------------------------------------
+//Pop up stuff;
 const openEls = document.querySelectorAll("[data-open]");
 const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
+
+openEls.forEach(element => {
+    console.log(element);
+});
 
 for (const el of openEls) {
     el.addEventListener("click", function() {
@@ -33,6 +38,12 @@ document.addEventListener("keyup", e => {
         document.querySelector(".modal.is-visible").classList.remove(isVisible);
     }
 });
+//Cuentas preEchas
+var cuentas = [
+    { nombre: "Mali@hotmail.com", contraseña: 111, saldo: 200 },
+    { nombre: "Gera@hotmail.com", contraseña: 111, saldo: 290 },
+    { nombre: "Maui@hotmail.com", contraseña: 111, saldo: 67 }
+];
 
 var x = ["", 0];
 
@@ -52,4 +63,34 @@ function retirar(event) {
     //Se te dara un array con nombre x donde contenga [nombre=string,saldo=Number];
     //el resultado estan las indicaciones en la linea **4**
 
+}
+//get user pasword
+var pasa = false,
+    item = -1;
+
+function getInfo() {
+    let findPerson = infoArray();
+    findPerson[0] != "" ? document.getElementById("Login").href = "In.html" : ErrorIntentarDN();
+}
+
+function ErrorIntentarDN() {
+    let newStuf = document.createAttribute("div");
+}
+
+function infoArray() {
+    let input = document.getElementById("Name").value;
+    let inputContraseña = document.getElementById("Pas").value;
+    cuentas.forEach(((e, i) => {
+        e.nombre == input ? item = i : 0;
+    }));
+    if (item != -1) {
+        cuentas[item].contraseña == inputContraseña ? pasa = true : pasa = false;
+        if (pasa) {
+            return [cuentas[item].nombre, cuentas[item].saldo];
+        } else {
+            return x;
+        }
+    } else {
+        return x;
+    }
 }
