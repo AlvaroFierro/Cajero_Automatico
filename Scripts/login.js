42c23bbc1038773736cdc0af992ab9f1765683d0
Alvaro Fierro <business.afierro@gmail.com>
2021-10-18 15:20:45 -0500

    .

--- Scripts/login.js
+++
@@ -1,6 +0,0 @@
-// Arreglo con las cuentas de los usuarios
-var cuentas = [
-  { nombre: "Angel", saldo: "550", password: "Angel123", activo: false },
-  { nombre: "Luis", saldo: "450", password: "Luis213", activo: false },
-  { nombre: "Alvaro", saldo: "350", password: "Alvaro312", activo: false },
-];
